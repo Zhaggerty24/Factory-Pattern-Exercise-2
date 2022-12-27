49d82131074e22562332363d652e91495ce5452c
Zachary Haggerty <zhaggerty24@gmail.com>
2022-12-26 19:18:32 -0500

    initial commit

---
+++ FactoryPatternExercise2/MongoDataAccess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+   public class MongoDataAccess : IDataAccess
+    {
+        public static List<Product> products = new List<Product>()
+        {
+            new Product(){Name = "Xbox" , Price = 499},
+            new Product(){Name = "Bike" , Price = 150},
+            new Product(){Name = "Lightsaber" , Price = 650},
+            new Product(){Name = "Sneakers" , Price = 120},
+        };
+        public List<Product> LoadData()
+        {
+            Console.WriteLine("I am reading data from the Mongo Data Access.");
+
+            return products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("I am saving data to the Mongo Data Access.");
+        }
+
+        
+    }
+}
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+            
+                Console.WriteLine("What database would you like to work with");
+                Console.WriteLine("Type: list");
+                Console.WriteLine("Type: sql");
+                Console.WriteLine("Type: mongo");
+
+                var userInput = Console.ReadLine();
+            
+            IDataAccess db = DataAccessFactory.GetDataAccessType(userInput);
+
+            db.SaveData();
+            var products = db.LoadData();
+
+            foreach(var item in products)
+            {
+                Console.WriteLine($"Name: {item.Name} Price: {item.Price}");
+            }
         }
     }
 }
